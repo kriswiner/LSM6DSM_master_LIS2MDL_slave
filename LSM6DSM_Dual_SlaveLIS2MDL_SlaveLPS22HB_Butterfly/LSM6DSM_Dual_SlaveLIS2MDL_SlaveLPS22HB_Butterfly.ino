5296ab59309e63cfafbd9050be93d7abde5a3f7c
Kris Winer <kriswiner@users.noreply.github.com>
2019-06-26 17:09:15 -0700

    Update LSM6DSM_Dual_SlaveLIS2MDL_SlaveLPS22HB_Butterfly.ino

--- LSM6DSM_Dual_SlaveLIS2MDL_SlaveLPS22HB_Butterfly/LSM6DSM_Dual_SlaveLIS2MDL_SlaveLPS22HB_Butterfly.ino
+++ LSM6DSM_Dual_SlaveLIS2MDL_SlaveLPS22HB_Butterfly/LSM6DSM_Dual_SlaveLIS2MDL_SlaveLPS22HB_Butterfly.ino
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2019 Tlera Corp.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimers.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimers in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of Tlera Corp, nor the names of its contributors
+ *     may be used to endorse or promote products derived from this Software
+ *     without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE.
+ *
+ * Example of using two LSM6DSM accel/gyro combo IMUs on the same I2C bus including configuring the LSM6DSMs
+ * as masters to slave LIS2MDL magnetometers and LPS22HB barometers, calculation of absolute orientation using Madgwick's
+ * open-source sensor fusion, and demonstrating ultra-low-power sleep/wake behavior.
+ */
 #include "LSM6DSM.h"
 #include "LIS2MDL.h"
 #include "LPS22HB.h"
